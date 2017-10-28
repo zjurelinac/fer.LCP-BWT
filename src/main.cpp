251fdb066a66bcae34c1c18e432059fe62854b31
Zvonimir Jurelinac <zjurelinac@gmail.com>
2017-10-28 20:29:12 +0200

    Improving main.cpp code style, reordering functions for readability

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@
 #include "wavelet_tree.hpp"
 #endif
 
-std::string parse_input(char* input_file);  // TODO: @Zivec
+std::string parse_input(char* input_file);
 void output_results(char* output_file, lb::lcp_array& LCP);
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cout << "Usage: lcp_bwt input_file output_file\n";
-        return -1;
+        exit(1);
     }
 
     lb::sequence T {"ananas$"};
@@ -28,6 +28,24 @@ int main(int argc, char* argv[]) {
     output_results(argv[2], LCP);
 }
 
+std::string parse_input(char* input_file) {
+    std::string input, temp;
+    std::ifstream file(input_file);
+
+    if (!file) {
+        std::cerr << "Unable to open file\n";
+        exit(1);
+    }
+
+    std::getline(file, temp);  // first line
+    while (!file.eof()) {
+        std::getline(file, temp);
+        input += temp;
+    }
+    file.close();
+    return input;
+}
+
 void output_results(char* output_file, lb::lcp_array& lcp) {
     std::ofstream ofs(output_file, std::ofstream::out);
     ofs << "[";
@@ -35,22 +53,4 @@ void output_results(char* output_file, lb::lcp_array& lcp) {
         ofs << lcp[i] << ((i != lcp.size() - 1) ? "," : "");
     ofs << "]\n";
     ofs.close();
-}
-std::string parse_input(char* input_file){
-    std::string input, temp;
-    std::ifstream file(input_file);
-
-    if (!file) {
-        std::cerr << "Unable to open file" <<std::endl;
-        exit(1);
-    }
-
-    std::getline(file, temp); // first line
-
-    while(!file.eof()){
-        std::getline(file, temp);
-        input += temp;
-    }
-    file.close();
-    return input;
 }
