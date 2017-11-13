7fb70c16987a8114860898bbd87d0dec95ebfaae
tonco <tonko114@hotmail.com>
2017-11-13 15:33:51 +0100

    Add files via upload
    added functions and fixes

--- src/include/fast_bitvector.hpp
+++ src/include/fast_bitvector.hpp
@@ -1,21 +1,30 @@
 #ifndef FAST_BITVECTOR_HPP
 #define FAST_BITVECTOR_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <utility>
 #include <vector>
-
+#include <iterator>
 #include "base.hpp"
 
-namespace lb {
+namespace lb { 
     class fast_bitvector {
     public:
-	fast_bitvector(const lb::bitvector& bitv);
+	typedef bool value_type;
+	fast_bitvector();
+	fast_bitvector(std::vector<bool> bv);
         size_t rank(size_t index, bool bit = false);
         void push_back(const bool& val);
         size_t size();
+	std::vector<bool>::iterator begin() ;
     private:
         std::vector<bool> bv;
         std::vector<size_t> cs;
     };
+    using bitvector = fast_bitvector;
 }
 
 #endif
