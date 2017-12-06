b274661864d54e87fa00d46ab9a489154dbdbfe5
Zvonimir Jurelinac <zjurelinac@gmail.com>
2017-12-06 09:57:27 -0500

    Fastqueue bugfix, performance improvements missing

--- src/include/containers.hpp
+++ src/include/containers.hpp
@@ -5,7 +5,7 @@
 
 #define SMALLSTACK_SIZE 32
 #define FASTQUEUE_OPT_SIZE 4096
-#define FASTQUEUE_START_SIZE 32
+#define FASTQUEUE_START_SIZE 512
 
 namespace lb {
     template <typename T>
@@ -29,7 +29,7 @@ namespace lb {
         using block_type = std::pair<T*, lb::size_t>;
     public:
         fastqueue()
-            : front(0), back(0), size(0) {}
+            : front(0), back(FASTQUEUE_START_SIZE), size(0) {}
         inline void push(T elem);
 
         inline T pop();
@@ -38,11 +38,11 @@ namespace lb {
             { return size == 0; }
     private:
         size_t new_block_size()
-            { return size ? 16 : FASTQUEUE_START_SIZE; }
+            { return size ? 512 : FASTQUEUE_START_SIZE; }
         void add_block(size_t size)
-            { printf("Adding block of size %u\n", size); list.emplace_front(new T[size], size); front = size; }
+            { list.emplace_front(new T[size], size); front = size; }
         void remove_block()
-            { printf("Removing block of size %u\n", list.back().second); delete[] list.back().first; list.pop_back(); back = list.back().second; }
+            { delete[] list.back().first; list.pop_back(); back = list.back().second; }
         size_t front, back, size;
         std::list<block_type> list;
     };
@@ -50,18 +50,17 @@ namespace lb {
 
 template <typename T>
 void lb::fastqueue<T>::push(T elem) {
-    if (size == 0 || front == 0)
+    if (front == 0)
         add_block(new_block_size());
-    if (!size)
-        back = FASTQUEUE_START_SIZE;
     (list.front().first)[--front] = elem;
     ++size;
 }
 
 template <typename T>
 T lb::fastqueue<T>::pop() {
+    --size;
+    auto t = (list.back().first)[--back];
     if (back == 0)
         remove_block();
-    --size;
-    return (list.back().first)[--back];
+    return t;
 }
--- tests/debug/fq.cpp
+++ tests/debug/fq.cpp
@@ -1,14 +1,58 @@
 #include "../../src/include/containers.hpp"
 
 #include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <queue>
+
+using par = std::pair<int, int>;
 
 int main() {
+    //lb::fastqueue<par> fq;
+    //std::queue<par> q;
+
     lb::fastqueue<int> fq;
-    for (int i = 0; i < 50; ++i)
+    std::queue<int> q;
+
+    srand(1);
+
+    /*for (int i = 0; i < 10000; ++i) {
+        int x = rand() % 100;
+        fq.push(par(x, 1));
+        q.push(par(x, 1));
+    }
+
+    for (int i = 0; i < 10000; ++i) {
+        auto x = fq.pop(), y = q.front(); q.pop();
+        if (x.first != y.first) 
+            printf("ERROR: items differ at %d: %d <> %d\n", i, x.first, y.first);
+    }*/
+    const int N = 100;
+
+    for (int i = 0; i < N; ++i) {
+        auto op = rand() % 2 == 0;
+        if (q.empty() != fq.empty())
+            printf("ERROR: empty difference: %d <> %d\n", q.empty(), fq.empty());
+        if (op) { // push
+            int x = rand() % 100;
+            printf("%d :: Pushing %d\n", i, x);
+            q.push(x); fq.push(x);
+        } else { // pop
+            if (!q.empty()) {
+                auto x = fq.pop(), y = q.front(); q.pop();
+                if (x != y)
+                    printf("ERROR: pop difference: %d <> %d\n", x, y);
+                else
+                    printf("%d :: Popping %d\n", i, x);
+            }
+        }
+    }
+
+    /*for (int i = 0; i < 50; ++i)
         fq.push(i);
 
     for (int i = 0; i < 50; ++i)
-        printf("%d ", fq.pop());
+        std::cout << fq.pop() << " ";
 
-    printf("\nempty? %d", fq.empty());
+    std::cout << "\nempty? " << fq.empty() << std::endl;*/
 }
